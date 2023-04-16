b489a8d1bf553e9001b803f9c17d048fbad51fb6
FoxOfNight <costyender31@gmail.com>
2023-04-16 15:46:45 +0300

    stuff

--- OpenGL Course - Making a window - exercises/main.cpp
+++ OpenGL Course - Making a window - exercises/main.cpp
@@ -13,8 +13,8 @@ int main() {
 	// Tell GLFW we are using the CORE profile (meaning modern functions only)
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	// Create a GLFW object of 800 by 800 pixels, naming it "OpenGL Course"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Course", NULL, NULL);
+	// Create a GLFW object
+	GLFWwindow* window = glfwCreateWindow(400, 225, "I made this!", NULL, NULL);
 	// Check if window was created, return -1 otherwise
 	if (window == NULL) {
 		std::cout << "Failed to create GLFW window\n";
@@ -29,19 +29,22 @@ int main() {
 
 	// Specify the viewport of OpenGL in the window
 	// In this case the viewport goes from x = 0, y = 0 to x = 800, y = 800 
-	glViewport(0, 0, 800, 800);
+	glViewport(0, 0, 400, 225);
 
 	// Specify the color of the background
-	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	glClearColor(0.75f, 0.25f, 0.0f, 1.0f);
 	// Clean the back buffer and assign the new	color to it
 	glClear(GL_COLOR_BUFFER_BIT);
 	// Swap the back buffer with the front buffer
 	glfwSwapBuffers(window);
+	glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT);
 
 	// Main while loop
 	while (!glfwWindowShouldClose(window)) {
 		// Do all GLFW events
 		glfwPollEvents();
+		glfwSwapBuffers(window);
 	}
 
 	// Delete window
