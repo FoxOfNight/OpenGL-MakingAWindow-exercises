d7fc4996aec04f83af4550e138e88289aabbed0d
FoxOfNight <costyender31@gmail.com>
2023-04-16 14:52:01 +0300

    Add Code

---
+++ OpenGL Course - Making a triangle/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+	// Initialize GLFW
+	glfwInit();
+
+	// Tell GLFW what versioan of OpenGL we are using
+	// In this case we are using OpenGL 3.3
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	// Tell GLFW we are using the CORE profile (meaning modern functions only)
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Create a GLFW object of 800 by 800 pixels, naming it "OpenGL Course"
+	GLFWwindow* window = glfwCreateWindow(800, 800, "OpenGL Course", NULL, NULL);
+	// Check if window was created, return -1 otherwise
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window\n";
+		glfwTerminate();
+		return -1;
+	}
+	// Add the window to the current context
+	glfwMakeContextCurrent(window);
+
+	// Load GLAD so it configures OpenGL
+	gladLoadGL();
+
+	// Specify the viewport of OpenGL in the window
+	// In this case the viewport goes from x = 0, y = 0 to x = 800, y = 800 
+	glViewport(0, 0, 800, 800);
+
+	// Specify the color of the background
+	glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+	// Clean the back buffer and assign the new	color to it
+	glClear(GL_COLOR_BUFFER_BIT);
+	// Swap the back buffer with the front buffer
+	glfwSwapBuffers(window);
+
+	// Main while loop
+	while (!glfwWindowShouldClose(window)) {
+		// Do all GLFW events
+		glfwPollEvents();
+	}
+
+	// Delete window
+	glfwDestroyWindow(window);
+	// Terminate GLFW
+	glfwTerminate();
+	return 0;
+}
